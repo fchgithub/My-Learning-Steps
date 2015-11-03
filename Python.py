26ad3b37281475f2624c43cd63521a4a3cb7a6e7
Fatemeh Cheraghchi <fchgithub@users.noreply.github.com>
2015-11-03 15:11:32 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -10,3 +10,16 @@ capital_country = {"United States" : "Washington",
                    "Netherlands" : "Amsterdam"}
 for c in capital_country:
     print("{country:13s} : {capital}".format(country=c, capital=capital_country[c]))
+    
+    
+for c in capital_country:
+    print('The %10d' %3434)
+
+    
+for c in capital_country:
+    print('The {c:10d}'.format(c = 131324) )
+
+for c in capital_country:
+    print('The {:10d} and {:3s}'.format(234131324, 'sdsds') )
+
+
